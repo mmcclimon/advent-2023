99bb170c40cdeef9abfdc1e3efb96d1701905560
Michael McClimon <michael@mcclimon.org>
2023-12-03 08:55:08 -0500

    day 3: do some small cleanups
    This still isn't ideal, but at least the return values are less weird.

--- bin/day03.py
+++ bin/day03.py
@@ -4,7 +4,6 @@ import re
 
 def main():
     lines = list(input.lines())
-    symbols = {}
 
     symbols = {
         (r, c): char
@@ -13,53 +12,46 @@ def main():
         if not char.isdigit() and char != "."
     }
 
-    gears = {tup: [] for tup in symbols if symbols[tup] == "*"}
+    gears = {coords: [] for coords, sym in symbols.items() if sym == "*"}
 
     sum1 = 0
-    sum2 = 0
 
     for r, line in enumerate(lines):
         for match in re.finditer(r"\b(\d+)\b", line):
-            found, gears_touched = is_part_number(symbols, r, match)
-            if not found:
+            coord = is_part_number(symbols, r, match)
+            if not coord:
                 continue
 
-            sum1 += int(match.group())
+            part_num = int(match.group())
+            sum1 += part_num
 
-            for gear in gears_touched:
-                gears[gear].append(int(match.group()))
+            if coord in gears:
+                gears[coord].append(part_num)
 
-    for gear, matches in gears.items():
-        if len(matches) == 2:
-            sum2 += matches[0] * matches[1]
+    sum2 = sum(parts[0] * parts[1] for parts in gears.values() if len(parts) == 2)
 
     print(f"part 1: {sum1}")
     print(f"part 2: {sum2}")
 
 
-# is part, set[gears touched]
+# returns coordinates of symbol it touches, if any
 def is_part_number(symbols, r, match):
-    found = False
-    found_gears = set()
-
     for c in range(*match.span()):
-        if tup := touches_symbol(symbols, r, c):
-            found = True
-            sym, coord = tup
-            if sym == "*":
-                found_gears.add(coord)
+        if coord := touches_symbol(symbols, r, c):
+            return coord
 
-    return found, found_gears
+    return
 
 
+# returns the coordinates of the thing it touches
 def touches_symbol(symbols, r, c):
     for rx in [-1, 0, 1]:
         for cx in [-1, 0, 1]:
             r2, c2 = r + rx, c + cx
             try:
-                if sym := symbols[(r2, c2)]:
-                    # (I manually verified every number only touches 1 symbol)
-                    return sym, (r2, c2)
+                # (I manually verified every number only touches 1 symbol)
+                if symbols[(r2, c2)]:
+                    return r2, c2
             except KeyError:
                 pass
 
