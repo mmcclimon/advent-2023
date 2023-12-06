6e441cf4654cd0c18d66a4e51f652fbeb01cef37
Michael McClimon <michael@mcclimon.org>
2023-12-06 18:21:04 -0500

    day 6: silly micro-optimizations
    Absolutely no reason for this.

--- bin/day06.py
+++ bin/day06.py
@@ -13,28 +13,22 @@ def main():
 def part_one(lines):
     times = [int(n) for n in lines[0].removeprefix("Time:").split()]
     dists = [int(n) for n in lines[1].removeprefix("Distance:").split()]
-    total = []
-
-    for i in range(len(times)):
-        time, d = times[i], dists[i]
-        total.append(do_race(time, d))
-
-    print(reduce(operator.mul, total))
+    totals = (do_race(times[i], dists[i]) for i in range(len(times)))
+    print(f"part 1: {reduce(operator.mul, totals)}")
 
 
 def part_two(lines):
     time = int(lines[0].removeprefix("Time:").replace(" ", ""))
     dist = int(lines[1].removeprefix("Distance:").replace(" ", ""))
 
-    print(do_race(time, dist))
+    print(f"part 2: {do_race(time, dist)}")
 
 
 # returns number of wins
 def do_race(time, record) -> int:
     wins = 0
     for t in range(1, time):
-        dist = (time - t) * t
-        if dist > record:
+        if t * (time - t) > record:
             wins += 1
 
     return wins
