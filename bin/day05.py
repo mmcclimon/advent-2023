5c4d79262f70af5864423dc6c486475ff8366126
Michael McClimon <michael@mcclimon.org>
2023-12-06 18:24:54 -0500

    WIP: day 5 nonsense unworking solution

--- bin/day05.py
+++ bin/day05.py
@@ -2,25 +2,40 @@ from advent import input
 import collections
 import itertools
 import re
+from dataclasses import dataclass
 
 
 def main():
     seeds, *hunks = input.hunks()
     seeds = [int(n) for n in seeds[0].removeprefix("seeds: ").split()]
 
-    global transformer
-    transformer = make_transformer(hunks)
-    # print(min(transformer(seed) for seed in munge_seeds(seeds)))
+    # transformer = make_transformer(hunks)
+    # print(min(transformer(seed) for seed in seeds))
 
-    for seed in munge_seeds(seeds):
-        v = transformer(seed)
-        print(f"{seed} -> {v}")
+    part_two(seeds, hunks)
+
+
+# note: inclusive!
+Range = collections.namedtuple('Range', ['start', 'end'])
 
 
 def make_transformer(hunks):
-    lookups = []
+    lookups = make_lookups(hunks)
 
-    Val = collections.namedtuple('Key', ['dst', 'src', 'range'])
+    def fn(item):
+        for lookup in lookups:
+            for k, val in lookup.items():
+                if k <= item < k + val.span:
+                    item = val.dst + (item - val.src)
+                    break
+
+        return item
+
+    return fn
+
+
+def make_lookups(hunks):
+    lookups = []
 
     for hunk in hunks:
         match = re.match(r"[a-z]+-to-(\w+) map", hunk[0])
@@ -31,49 +46,60 @@ def make_transformer(hunks):
         lookup = {}
 
         for line in hunk[1:]:
-            dst, src, range = (int(n) for n in line.split())
-            lookup[src] = Val(dst, src, range)
+            dst, src, span = (int(n) for n in line.split())
+            lookup[src] = Almanac(dst, src, span)
 
         lookups.append(lookup)
 
-    def fn(item):
-        for lookup in lookups:
-            for k, val in lookup.items():
-                if k <= item < k + val.range:
-                    item = val.dst + (item - val.src)
-                    break
-
-        return item
-
-    return fn
+    return lookups
 
 
-def part_two(seeds):
+def part_two(seeds, hunks):
+    lookups = make_lookups(hunks)
+    print(lookups)
+
     for start, span in itertools.batched(seeds, 2):
-        best_for_range(start, span)
+        end = start + span - 1
+        for stage in lookups:
+            for a in stage.values():
+                print(f"checking {start=}, {end=}, {a=}")
+                if end < a.input.start:
+                    print("left")
+                elif a.input.end < start:
+                    print("right")
+                elif a.input.start <= start and end <= a.input.end:
+                    print("contained")
+                elif start < a.input.start:
+                    print(f"dangle left; queue: {start}, {a.input.start}, {end}")
+                elif start < a.input.end:
+                    print(f"dangle right; queue: {start}, {a.input.end}, {a.input.end+1}, {end}")
+                else:
+                    print("dunno")
+                    raise RuntimeError
+
+            print("----")
 
 
-def best_for_range(start, span):
-    end = start + span - 1
-    print(start, end)
-    have = collections.deque()
+@dataclass
+class Almanac:
+    dst: int
+    src: int
+    span: int
 
-    global transformer
-    have.append(transformer(start))
-    have.append(transformer(end))
-    print(have)
+    @property
+    def input(self) -> Range:
+        return Range(self.src, self.src + self.span - 1)
 
-    # print("start stop:", transformer(start), transformer(end))
+    @property
+    def output(self) -> Range:
+        return Range(self.dst, self.dst + self.span - 1)
 
-    # binary search over the span?
-    pass
+    def map(self, n) -> int:
+        if n < self.input.start or n > self.input.end:
+            return n
 
+        return n + self.span
 
-def munge_seeds(seeds):
-    for start, span in itertools.batched(seeds, 2):
-        print(f"starting span {start}")
-        for i in range(start, start + span):
-            yield i
 
 
 if __name__ == "__main__":
