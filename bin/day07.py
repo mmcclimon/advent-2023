54d2cdc187d7ae5df0424508108ed01e2b8203ac
Michael McClimon <michael@mcclimon.org>
2023-12-07 10:00:01 -0500

    day 7: part 2, not so bad
    I turned myself around a bit thinking too hard about this, but it
    actually wasn't really that difficult as I wanted it to be.

--- bin/day07.py
+++ bin/day07.py
@@ -1,14 +1,23 @@
 from advent import input
 from collections import Counter
 from enum import IntEnum
+import functools
 
 
 def main():
+    lines = list(input.lines())
+
     sum = 0
-    for i, hand in enumerate(sorted(Hand(line) for line in input.lines())):
+    for i, hand in enumerate(sorted(Hand(line) for line in lines)):
         sum += hand.bid * (i + 1)
 
-    print(sum)
+    print(f"part 1: {sum}")
+
+    sum2 = 0
+    for i, hand in enumerate(sorted(WildHand(line) for line in lines)):
+        sum2 += hand.bid * (i + 1)
+
+    print(f"part 2: {sum2}")
 
 
 class Rank(IntEnum):
@@ -19,6 +28,19 @@ class Rank(IntEnum):
     FULL = 5
     FOUR = 6
     FIVE = 7
+
+    @classmethod
+    def for_hand(cls, counts):
+        if 5 in counts:
+            return Rank.FIVE
+        elif 4 in counts:
+            return Rank.FOUR
+        elif 3 in counts:
+            return Rank.FULL if 2 in counts else Rank.THREE
+        elif 2 in counts:
+            return Rank.TWO_PAIR if Counter(counts)[2] == 2 else Rank.PAIR
+        else:
+            return Rank.HIGH
 
 
 class Hand:
@@ -36,32 +58,43 @@ class Hand:
         self.bid = int(bid)
 
     def __repr__(self):
-        return f"<Hand hand={self.hand} bid={self.bid}, rank={self.rank()}>"
+        return f"<Hand hand={self.hand} bid={self.bid}, rank={self.rank}>"
 
     def __lt__(self, other):
-        me, them = self.rank(), other.rank()
+        me, them = self.rank, other.rank
         if me == them:
             for i in range(5):
                 if self.hand[i] == other.hand[i]:
                     continue
 
-                return self.Values[self.hand[i]] < self.Values[other.hand[i]]
+                return self.value_for(self.hand[i]) < self.value_for(other.hand[i])
 
         return me < them
 
+    @functools.cached_property
     def rank(self) -> Rank:
-        counts = list(Counter(self.hand).values())
+        return Rank.for_hand(Counter(self.hand).values())
 
-        if 5 in counts:
-            return Rank.FIVE
-        elif 4 in counts:
-            return Rank.FOUR
-        elif 3 in counts:
-            return Rank.FULL if 2 in counts else Rank.THREE
-        elif 2 in counts:
-            return Rank.TWO_PAIR if Counter(counts)[2] == 2 else Rank.PAIR
-        else:
-            return Rank.HIGH
+    def value_for(self, val):
+        return self.Values[val]
+
+
+class WildHand(Hand):
+    @functools.cached_property
+    def rank(self) -> Rank:
+        counts = Counter(self.hand)
+        jokers = counts.pop("J", 0)
+
+        if not jokers or jokers == 5:
+            return super().rank
+
+        # Add the jokers to whatever the most cards we have is
+        vals = sorted(counts.values(), reverse=True)
+        vals[0] += jokers
+        return Rank.for_hand(vals)
+
+    def value_for(self, val):
+        return 1 if val == "J" else self.Values[val]
 
 
 if __name__ == "__main__":
