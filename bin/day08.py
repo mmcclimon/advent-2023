f9296c9f8d18a0bd35748c35d8a78b3c72ec13db
Michael McClimon <michael@mcclimon.org>
2023-12-08 17:28:17 -0500

    day 8: clean up just a bit
    I don't actually need the special predicate, I can just always use
    .endswith('Z'), because 'AAA' is the start of the path ending at 'ZZZ'.
    And if we don't need the predicate, then passing the directions and map
    to the search function directly looks much less gross, so get rid of the
    namedtuple.

--- bin/day08.py
+++ bin/day08.py
@@ -1,5 +1,4 @@
 from advent import input, mathutil
-from collections import namedtuple
 from functools import reduce
 import itertools
 
@@ -13,34 +12,27 @@ def main():
         l, r = locs[1:-1].split(", ")
         map[key] = (l, r)
 
-    legend = namedtuple('Legend', ["directions", "map"])(directions, map)
+    part1 = search(directions, map, "AAA")
 
-    part_one(legend)
-    part_two(legend)
+    part2 = reduce(
+        mathutil.lcm,
+        (search(directions, map, node) for node in map if node.endswith("A")),
+    )
 
-
-def part_one(legend):
-    moves = search(legend, "AAA", lambda n: n == "ZZZ")
-    print(f"part 1: {moves}")
-
-
-def part_two(legend):
-    starts = [node for node in legend.map if node.endswith("A")]
-    zs = [search(legend, node, lambda n: n.endswith("Z")) for node in starts]
-
-    print(f"part 2: {reduce(mathutil.lcm, zs)}")
+    print(f"part 1: {part1}")
+    print(f"part 2: {part2}")
 
 
 # Returns length of the path
-def search(legend, start, pred) -> int:
+def search(directions, map, start) -> int:
     moves = 0
     cur = start
 
-    for dir in itertools.cycle(legend.directions):
+    for dir in itertools.cycle(directions):
         moves += 1
-        locs = legend.map[cur]
+        locs = map[cur]
         cur = locs[0] if dir == "L" else locs[1]
-        if pred(cur):
+        if cur.endswith("Z"):
             return moves
 
     assert False
