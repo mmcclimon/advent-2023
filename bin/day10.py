5cf6a4bf96bf30ff1cf016804b4f119003c36fb1
Michael McClimon <michael@mcclimon.org>
2023-12-10 15:25:04 -0500

    day 10: add part 2 solution
    I think this solution is fairly clever, even if I wrote a skrillion bugs
    while doing it.

--- bin/day10.py
+++ bin/day10.py
@@ -9,19 +9,31 @@ def main():
         for c, char in enumerate(line):
             grid.add(r, c, Pipe(char))
 
-    grid.find_loop()
+    print("part 1:", grid.find_loop() // 2)
+    print("part 2:", grid.num_inside_squares())
 
 
-Dir = Enum("Dir", ["N", "E", "S", "W"])
+class Dir(Enum):
+    N = "N"
+    E = "E"
+    S = "S"
+    W = "W"
 
+    def opposite(self):
+        return {
+            Dir.N: Dir.S,
+            Dir.S: Dir.N,
+            Dir.E: Dir.W,
+            Dir.W: Dir.E,
+        }[self]
 
-def opposite_dir(direction):
-    return {
-        Dir.N: Dir.S,
-        Dir.S: Dir.N,
-        Dir.E: Dir.W,
-        Dir.W: Dir.E,
-    }[direction]
+    def rowcol(self, r, c):
+        return {
+            Dir.N: (r - 1, c),
+            Dir.S: (r + 1, c),
+            Dir.E: (r, c + 1),
+            Dir.W: (r, c - 1),
+        }[self]
 
 
 class Pipe:
@@ -33,12 +45,11 @@ class Pipe:
             "J": (Dir.N, Dir.W),
             "7": (Dir.S, Dir.W),
             "F": (Dir.S, Dir.E),
-            ".": (),
-            "S": (Dir.N, Dir.E, Dir.S, Dir.W),  # I might regret this
+            "S": (Dir.N, Dir.E, Dir.S, Dir.W),
         }
 
         self.char = char
-        self.connections = lookup[char]
+        self.connections = lookup.get(char, ())
         self.is_start = char == "S"
 
     def __repr__(self):
@@ -50,6 +61,45 @@ class Grid:
         self.grid = {}
         self.start = None
         self.loop = set()
+        self.outside = set()
+
+    def __str__(self):
+        self.find_loop()
+
+        rows = []
+        for r in range(self.min_r, self.max_r + 1):
+            row = []
+            for c in range(self.min_c, self.max_c + 1):
+                pipe = self.grid[(r, c)]
+                char = "."
+                if (r, c) in self.loop:
+                    char = pipe.char
+                elif (r, c) in self.outside:
+                    char = "O"
+                elif pipe.char != ".":
+                    char = "?"
+
+                row.append(char)
+
+            rows.append("".join(row))
+
+        return "\n".join(rows)
+
+    @property
+    def min_r(self):
+        return min(r for r, _ in self.grid)
+
+    @property
+    def max_r(self):
+        return max(r for r, _ in self.grid)
+
+    @property
+    def min_c(self):
+        return min(c for _, c in self.grid)
+
+    @property
+    def max_c(self):
+        return max(c for _, c in self.grid)
 
     def add(self, r, c, pipe):
         self.grid[(r, c)] = pipe
@@ -58,7 +108,7 @@ class Grid:
 
     def find_loop(self):
         if self.loop:
-            return
+            return len(self.loop)
 
         # in which I write yet another DFS algo.
         todo = [self.start]
@@ -67,32 +117,111 @@ class Grid:
             cur = todo.pop()
             if cur not in self.loop:
                 self.loop.add(cur)
-                for neighbor in self.neighbors_of(cur):
-                    todo.append(neighbor)
+                todo.extend(self.neighbors_of(cur))
 
-        print(len(self.loop) // 2)
+        return len(self.loop)
 
     def neighbors_of(self, coords):
         r, c = coords
 
-        neighbors = {
-            Dir.N: (r - 1, c),
-            Dir.S: (r + 1, c),
-            Dir.E: (r, c + 1),
-            Dir.W: (r, c - 1),
-        }
-
         ret = []
         for connection in self.grid[coords].connections:
             try:
-                nc = neighbors[connection]
-                neighbor = self.grid[nc]
-                if opposite_dir(connection) in neighbor.connections:
-                    ret.append(nc)
+                other = connection.rowcol(r, c)
+                if connection.opposite() in self.grid[other].connections:
+                    ret.append(other)
             except KeyError:
                 pass
 
         return ret
+
+    def num_inside_squares(self):
+        # Ok, the plan is: expand the grid to double-size (so that all the
+        # spaces between pipes become real tiles), then do a flood-fill DFS
+        # starting on all outside tiles. Then, all we need to do is
+        # re-contract the grid (by walking it two at a time), counting up the
+        # tiles that aren't in the loop or in the set of outside tiles.
+        big = self._expand()
+        big._find_exits()
+
+        total = 0
+
+        for r in range(0, big.max_r, 2):
+            for c in range(0, big.max_c, 2):
+                coords = (r, c)
+                if coords in big.loop or coords in big.outside:
+                    continue
+
+                total += 1
+
+        return total
+
+    # Zoom in! Enhance!
+    def _expand(self):
+        self.find_loop()
+
+        # Each tile in the original generates 4 tiles in the expansion:
+        # self, right, down, down-right
+        big = Grid()
+        for r in range(0, self.max_r + 1):
+            for c in range(0, self.max_c + 1):
+                r2, c2 = r * 2, c * 2
+
+                # down-right tile is never interesting
+                big.add(r2 + 1, c2 + 1, Pipe("."))
+
+                if (r, c) not in self.loop:
+                    big.add(r2, c2, Pipe("."))
+                    big.add(r2 + 1, c2, Pipe("."))
+                    big.add(r2, c2 + 1, Pipe("."))
+                    continue
+
+                # In the loop: top left is just our tile.
+                big.add(r2, c2, self.grid[(r, c)])
+
+                rxy = (r, c + 1)
+                right = Pipe("-") if rxy in self.neighbors_of((r, c)) else Pipe(".")
+                big.add(r2, c2 + 1, right)
+
+                dxy = (r + 1, c)
+                down = Pipe("|") if dxy in self.neighbors_of((r, c)) else Pipe(".")
+                big.add(r2 + 1, c2, down)
+
+        # Add an extra row on top and left, to make sure that we have a full
+        # perimeter. (The expansion ensures we have one bottom/right already.)
+        big.add(-1, -1, Pipe("."))
+
+        for r in range(0, big.max_r + 1):
+            big.add(r, -1, Pipe("."))
+
+        for c in range(0, big.max_c + 1):
+            big.add(-1, c, Pipe("."))
+
+        return big
+
+    # Do a DFS starting at a tile known to be outside. At the end of this
+    # method, self.outside is filled with all such tiles.
+    def _find_exits(self):
+        self.find_loop()
+
+        def relevant(r, c):
+            return [
+                coord
+                for coord in [(r - 1, c), (r + 1, c), (r, c + 1), (r, c - 1)]
+                if coord in self.grid and self.grid[coord].char == "."
+            ]
+
+        # I could reuse this algorithm, but also.
+        todo = [(-1, -1)]
+
+        while todo:
+            cur = todo.pop()
+
+            if cur not in self.outside:
+                self.outside.add(cur)
+                todo.extend(coord for coord in relevant(*cur))
+
+        return
 
 
 if __name__ == "__main__":
