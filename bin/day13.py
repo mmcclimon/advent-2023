9cb79870a6e6347fe7264b7a3b9798fed88d3a9b
Michael McClimon <michael@mcclimon.org>
2023-12-13 18:38:59 -0500

    day 13 part 1: refactor to make part 2 easier
    Don't bother with rows and columns, just count one and write a function
    to rotate the grid 90 degrees.

--- bin/day13.py
+++ bin/day13.py
@@ -11,30 +11,38 @@ def main():
 
 
 def process_hunk(hunk) -> int:
+    if rv := compute(hunk):
+        return 100 * rv
+
+    cv = compute(rotate(hunk))
+    assert cv is not None
+    return cv
+
+
+def compute(hunk) -> int | None:
     rows = defaultdict(list)
-    cols = defaultdict(list)
-    max_r, max_c = 0, 0
+    max_r = 0
 
     for r, row in enumerate(hunk):
         rows[row].append(r)
         max_r = r
 
-    for c in range(len(hunk[0])):
-        col = "".join(line[c] for line in hunk)
-        cols[col].append(c)
-        max_c = c
-
-    rv = compute_deltas(list(rows.values()), max_r)
-    cv = compute_deltas(list(cols.values()), max_c)
-
-    if rv:
-        return 100 * rv
-
-    assert cv is not None
-    return cv
+    return calc_deltas(list(rows.values()), max_r)
 
 
-def compute_deltas(matches, biggest):
+def rotate(hunk):
+    """Take a hunk and rotate it such that all columns become rows"""
+    grid = [list(line) for line in hunk]
+    rotated = [[""] * len(grid) for _ in range(len(grid[0]))]
+
+    for r, line in enumerate(grid):
+        for c, char in enumerate(line):
+            rotated[c][r] = char
+
+    return ["".join(lst) for lst in rotated]
+
+
+def calc_deltas(matches, biggest):
     lookup = {el: set(lst) - {el} for lst in matches for el in lst}
 
     for cur in range(0, biggest + 1):
