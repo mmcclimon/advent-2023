67e456a40971935f499ec8c8d72e1d0cddbb40e8
Michael McClimon <michael@mcclimon.org>
2023-12-14 12:37:53 -0500

    day 14: refactor part 1 to make part 2 easier
    That's my lunchtime coding, actual solution will wait.

--- bin/day14.py
+++ bin/day14.py
@@ -1,60 +1,74 @@
 from advent import input
+import itertools
 
 
 def main():
-    grid = {}
-    rows, cols = 0, 0
-    for r, line in enumerate(input.lines()):
-        rows += 1
-        col_count = 0
-        for c, char in enumerate(line):
-            grid[(r, c)] = char
-            col_count += 1
+    lines = list(input.lines())
+    p1 = Grid(lines)
+    p1.tilt("N")
+    print("part 1:", p1.total_load())
 
-        cols = col_count
 
-    # pg(grid)
+class Grid:
+    def __init__(self, lines):
+        self.n_rows = 0
+        self.n_cols = 0
+        self._grid = {}
 
-    for r in range(1, rows):
-        for c in range(cols):
-            if grid[(r, c)] != "O":
+        for r, line in enumerate(lines):
+            self.n_rows += 1
+            self.n_cols = 0
+            for c, char in enumerate(line):
+                self[r, c] = char
+                self.n_cols += 1
+
+    def __getitem__(self, idx):
+        return self._grid[idx]
+
+    def __setitem__(self, idx, val):
+        self._grid[idx] = val
+
+    def print(self):
+        for r in range(self.n_rows):
+            line = []
+            for c in range(self.n_cols):
+                line.append(self[r, c])
+
+            print("".join(line))
+
+    def total_load(self):
+        return sum(
+            self.n_rows - r
+            for r in range(self.n_rows)
+            for c in range(self.n_cols)
+            if self[r, c] == "O"
+        )
+
+    def tilt(self, dir):
+        def _next(coords):
+            r, c = coords
+            return {
+                "N": (r - 1, c),
+                "S": (r + 1, c),
+                "E": (r, c + 1),
+                "W": (r, c - 1),
+            }[dir]
+
+        # we must iterate in reverse order if tilting south or east
+        wrap = reversed if dir in "SE" else itertools.chain
+
+        for cur, char in wrap(self._grid.items()):
+            if char != "O":
                 continue
 
-            cur = r
-            while cur >= 1:
-                if grid[(cur - 1, c)] != ".":
+            nxt = _next(cur)
+            while nxt in self._grid:
+                if self[nxt] != ".":
                     break
 
-                # print(f"moving from {(cur, c)} to {(cur-1, c)}")
-
-                grid[(cur - 1, c)] = "O"
-                grid[(cur, c)] = "."
-                cur -= 1
-
-    # pg(grid)
-
-    total = 0
-
-    for r in range(rows):
-        for c in range(cols):
-            if grid[(r, c)] != "O":
-                continue
-
-            total += rows - r
-
-    print(total)
-
-
-def pg(grid):
-    row = max(r for r, _ in grid)
-    col = max(c for _, c in grid)
-
-    for r in range(row + 1):
-        line = []
-        for c in range(col + 1):
-            line.append(grid[(r, c)])
-
-        print("".join(line))
+                self[nxt] = "O"
+                self[cur] = "."
+                cur, nxt = nxt, _next(nxt)
 
 
 if __name__ == "__main__":
