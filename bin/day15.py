ffd7524bfd6e2b0f4b82987c3ca6bc8602c55462
Michael McClimon <michael@mcclimon.org>
2023-12-15 11:17:45 -0500

    day 15: add part 2 solution
    This was...very easy? Maybe later I'll implement a "real" hashmap, without
    using python dicts.

--- bin/day15.py
+++ bin/day15.py
@@ -1,4 +1,5 @@
 from advent import input
+from collections import defaultdict
 
 
 def main():
@@ -7,13 +8,32 @@ def main():
     p1 = sum(hash(bit) for bit in line.split(","))
     print(p1)
 
+    # part 2
+    h = defaultdict(dict)
+
+    for bit in line.split(","):
+        if bit[-1] == "-":
+            label = bit[:-1]
+            k = hash(label)
+            if label in h[k]:
+                del h[k][label]
+        else:
+            label, val = bit.split("=")
+            k = hash(label)
+            h[k][label] = val
+
+    p2 = sum(
+        (k + 1) * (i + 1) * int(val)
+        for k, d in h.items()
+        for i, val in enumerate(d.values())
+    )
+    print(p2)
+
 
 def hash(s):
     cur = 0
-
     for c in s:
         cur = ((cur + ord(c)) * 17) % 256
-        # print(f"after {c}: {cur}")
 
     return cur
 
