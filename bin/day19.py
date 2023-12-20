13afbb80189301a1abcd915955c8c0cc9475a569
Michael McClimon <michael@mcclimon.org>
2023-12-19 22:22:07 -0500

    day 19: fix the bug that made this not work
    And delete a bunch of garbage test code.

--- bin/day19.py
+++ bin/day19.py
@@ -1,5 +1,6 @@
 from advent import input
 import copy
+from functools import reduce
 import operator
 import re
 
@@ -15,9 +16,12 @@ def main():
         rules = [Rule(r) for r in m.group(2).split(",")]
         flows[m.group(1)] = rules
 
-    total = 0
+    print("part 1:", part_one(flows, ps))
+    print("part 2:", analyze_flows(flows))
 
-    analyze_flows(flows)
+
+def part_one(flows, ps):
+    total = 0
 
     for p in ps:
         match = re.match(r"{x=(\d+),m=(\d+),a=(\d+),s=(\d+)}", p)
@@ -27,7 +31,7 @@ def main():
         if should_accept(flows, part):
             total += sum(part.values())
 
-    # print(total)
+    return total
 
 
 def should_accept(flows, part) -> bool:
@@ -49,10 +53,7 @@ def should_accept(flows, part) -> bool:
 
 
 def analyze_flows(flows):
-    # for label, rules in flows.items():
-    #     outs = {r.out for r in rules}
-    #     print(f"{label} -> {{{' '.join(outs)}}}")
-
+    # This is obviously stupid, but works because n is small.
     ranges = {
         "x": set(range(1, 4001)),
         "m": set(range(1, 4001)),
@@ -65,129 +66,40 @@ def analyze_flows(flows):
     seen = set()
     S.append(("in", ranges))
 
-    outs = {"A": set(), "R": set()}
     total = 0
 
     while S:
         label, ranges = S.pop()
-        print(f"looking at {label}")
 
-        if label == "R":
-            # print("reject")
+        if label in seen:
+            continue
+
+        if label == "R":  # reject
             continue
 
         if label == "A":
-            result = (
-                len(ranges["x"])
-                * len(ranges["m"])
-                * len(ranges["a"])
-                * len(ranges["s"])
-            )
-            print("accept (TODO)", result)
-            total += result
+            total += reduce(operator.mul, (len(ranges[k]) for k in "xmas"))
             continue
 
-        if label not in seen:
-            seen.add(label)
+        seen.add(label)
 
-            for rule in flows[label]:
-                rangecopy = copy.deepcopy(ranges)
-                if rule.op is None:
-                    S.append((rule.out, rangecopy))
-                else:
-                    p = rule.param
-                    for num in [n for n in rangecopy[p] if not rule.apply_true(n)]:
-                        # print(f"removing {p}={num}")
-                        rangecopy[p].remove(num)
-                    S.append((rule.out, rangecopy))
-
-    print(total)
-
-    # for l2 in [r.out for r in flows[label]]:
-    #     if l2 == "R":
-    #         continue
-
-    #     if l2 == "A":
-    #         print("found an out")
-    #         break
-
-    #     S.append((l2, list(path)))
-
-    # start with literally everything
-    ranges = {
-        "x": set(range(1, 4001)),
-        "m": set(range(1, 4001)),
-        "a": set(range(1, 4001)),
-        "s": set(range(1, 4001)),
-    }
-
-    total = 0
-
-    for path in sorted(outs["A"]):
-        # generate the set of sets for this path
-        n, ranges = examine_path(flows, ranges, path)
-        # print(ranges)
-        total += n
-
-    # total = len(ranges["x"]) * len(ranges["m"]) * len(ranges["a"]) * len(ranges["s"])
-    print(total)
-
-    # for every distinct path to A
-    # walk from A -> in
-    # construct condition chain
-
-
-def examine_path(flows, _ranges, path):
-    print(path)
-
-    ranges = {
-        "x": set(range(1, 4001)),
-        "m": set(range(1, 4001)),
-        "a": set(range(1, 4001)),
-        "s": set(range(1, 4001)),
-    }
-
-    for i in range(len(path) - 1):
-        label = path[i]
-        to = path[i + 1]
-        rules = flows[label]
-        print("   ", label, flows[label])
-
-        for rule in rules:
-            if rule.out == to:
-                # print(f"RULE: {rule}")
-                fn = rule.apply_true
+        for rule in flows[label]:
+            if rule.op is None:
+                S.append((rule.out, ranges))
             else:
-                # print(f"RULE: NOT {rule}")
-                fn = rule.apply_false
+                p = rule.param
 
-            match rule.op:
-                case None:
-                    pass
-                case operator.lt:
-                    p = rule.param
-                    for num in [n for n in ranges[p] if not fn(n)]:
-                        # print(f"removing {p}={num}")
-                        ranges[p].remove(num)
-                case operator.gt:
-                    p = rule.param
-                    for num in [n for n in ranges[p] if not fn(n)]:
-                        # print(f"removing {p}={num}")
-                        ranges[p].remove(num)
+                # for the path we're taking: remove all the false bits
+                rangecopy = copy.deepcopy(ranges)
+                for num in (n for n in ranges[p] if rule.apply_false(n)):
+                    rangecopy[p].discard(num)
+                S.append((rule.out, rangecopy))
 
-            if rule.out == to:
-                break
+                # for everything else: remove everything else
+                for num in [n for n in ranges[p] if rule.apply_true(n)]:
+                    ranges[p].discard(num)
 
-    print("   ", {k: len(ranges[k]) for k in ranges})
-
-    total = len(ranges["x"]) * len(ranges["m"]) * len(ranges["a"]) * len(ranges["s"])
-    print("   ", path, total)
-    return total, ranges
-
-
-# if the total range is 1-20
-# for s<10: 9 numbers.
-# for s>10: 10 numbers.
+    return total
 
 
 class Rule:
@@ -228,14 +140,3 @@ class Rule:
 
 if __name__ == "__main__":
     main()
-
-# 496534091000000
-# 167409079868000
-#  41256000000000
-#  48571397887500
-
-# ('in', 'px', 'A')
-#    in <Rule r=[s<1351:px]>
-#    px <Rule r=[m>2090:A]>
-# so, after this rule you should never have x=?, m>2090, a?!, s<135, m>2090
-# ('in', 'px', 'A') 41256000000000
